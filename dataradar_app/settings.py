eca3b773bf33c270cee0edf9109f694966495ce9
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2016-06-08 02:44:04 +0200

    correct error on db connect

--- dataradar_app/settings.py
+++ dataradar_app/settings.py
@@ -144,14 +144,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-# Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
