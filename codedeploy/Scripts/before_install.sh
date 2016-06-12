f5390107e20795d1e4db3a2dd3f403acdcc592a7
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2016-06-13 00:19:42 +0200

    right fix

--- codedeploy/Scripts/before_install.sh
+++ codedeploy/Scripts/before_install.sh
@@ -3,7 +3,7 @@
 app_dir="/home/ec2-user/dataradar"
 
 # Make directory for project
-rm -rf $app_dir
-mkdir -p $app_dir
-chown -R django:django $app_dir
-chmod -R 755 $app_dir
+sudo rm -rf $app_dir
+sudo mkdir -p $app_dir
+sudo chown -R django:django $app_dir
+sudo chmod -R 755 $app_dir
