cc34ece2c96d3d40739aabbdf1f82d90e6c8933a
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2016-06-12 23:46:20 +0200

    after_install & appspec scripts changed

--- codedeploy/Scripts/after_install.sh
+++ codedeploy/Scripts/after_install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-yum install python-pip wget postgresql gcc python-devel postgresql-devel screen -y
-pip install --upgrade pip
+sudo yum install python-pip wget postgresql gcc python-devel postgresql-devel screen -y
 
 app_dir="/home/ec2-user/dataradar"
 
@@ -11,9 +10,8 @@ source $app_dir/venv/bin/activate
 pip install --upgrade pip
 
 pip install -r $app_dir/requirements.txt
-
-wget -q https://s3-eu-west-1.amazonaws.com/feedreader-codedeploy/feedreader_data/data_.zip- O $app_dir/data.zip
-unzip -o $app_dir/data.zip
+sudo wget -q https://s3-eu-west-1.amazonaws.com/feedreader-codedeploy/feedreader_data/data_.zip -O $app_dir/data.zip
+sudo unzip -o $app_dir/data.zip
 python $app_dir/manage.py collectstatic --noinput
 python $app_dir/manage.py migrate --noinput
 python $app_dir/manage.py makemigrations --noinput
