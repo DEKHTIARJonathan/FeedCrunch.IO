44b0a3e04d5cea3aa50470eb3f289c3c01208c9f
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2016-06-13 00:17:40 +0200

    rights fix

--- codedeploy/Scripts/before_install.sh
+++ codedeploy/Scripts/before_install.sh
@@ -3,5 +3,7 @@
 app_dir="/home/ec2-user/dataradar"
 
 # Make directory for project
-rm -rf /home/ec2-user/dataradar
-mkdir -p /home/ec2-user/dataradar
+rm -rf $app_dir
+mkdir -p $app_dir
+chown -R django:django $app_dir
+chmod -R 755 $app_dir
