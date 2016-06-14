20d3b24c3606b32f509e778baeaa4af962cbc68a
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2016-06-14 20:52:53 +0200

    test avec ec2-user

--- codedeploy/Scripts/after_install.sh
+++ codedeploy/Scripts/after_install.sh
@@ -2,7 +2,8 @@
 
 sudo yum install python-pip wget postgresql gcc python-devel postgresql-devel screen libxslt-devel -y
 
-app_dir="/home/django/dataradar"
+app_dir="/home/ec2-user/dataradar"
+user="ec2-user"
 
 virtualenv $app_dir/venv
 source $app_dir/venv/bin/activate
@@ -11,7 +12,7 @@ pip install -r $app_dir/requirements.txt
 
 wget -q https://s3-eu-west-1.amazonaws.com/feedreader-codedeploy/feedreader_data/data_.zip -O $app_dir/data.zip
 sudo unzip -o $app_dir/data.zip -d $app_dir/
-sudo chown django:django $app_dir/.env
+sudo chown $user:$user $app_dir/.env
 sudo chmod 755 $app_dir/.env $app_dir/data.zip
 
 python $app_dir/manage.py collectstatic --noinput
--- codedeploy/Scripts/before_install.sh
+++ codedeploy/Scripts/before_install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-app_dir="/home/django/dataradar"
+app_dir="/home/ec2-user/dataradar"
+user="ec2-user"
 
 # Make directory for project
 sudo rm -rf $app_dir
 sudo mkdir -p $app_dir
-sudo chown -R django:django $app_dir
+sudo chown -R $user:$user $app_dir
 sudo chmod -R 755 $app_dir
