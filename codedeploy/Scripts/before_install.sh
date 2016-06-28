f868f05d4fa9d90953b8082d200887ebc73b83f1
DEKHTIAR Jonathan <contact@jonathandekhtiar.eu>
2016-06-28 19:41:12 +0200

    Code Deploy fix

--- codedeploy/Scripts/before_install.sh
+++ codedeploy/Scripts/before_install.sh
@@ -4,7 +4,7 @@ app_dir="/home/ec2-user/feedcrunch"
 user="ec2-user"
 
 # Make directory for project
-#sudo rm -rf $app_dir
+sudo rm -rf $app_dir
 sudo mkdir -p $app_dir
 sudo chown -R $user:$user $app_dir
 sudo chmod -R 755 $app_dir
