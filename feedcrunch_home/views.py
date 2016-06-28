d81b8471f5be0ef90bff8e2037764df740d3b902
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2016-06-28 23:48:28 +0200

    Home view fixed

--- feedcrunch_home/views.py
+++ feedcrunch_home/views.py
@@ -1,19 +1,19 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.shortcuts import render_to_response
 
 def index(request):
-    template = loader.get_template('index_home.html')
-    context = RequestContext(request, {} )
-    return HttpResponse(template.render(context))
-    #return render_to_response("index.html", {},context_instance=RequestContext(request))
+    response = render_to_response('index_home.html')
+    response.status_code = 200
+    return response
 
 def handler404(request):
-    response = render_to_response('404.html', {}, context_instance=RequestContext(request))
+    response = render_to_response('404.html')
     response.status_code = 404
     return response
 
 
 def handler500(request):
-    response = render_to_response('500.html', {}, context_instance=RequestContext(request))
+    response = render_to_response('500.html')
     response.status_code = 500
     return response
