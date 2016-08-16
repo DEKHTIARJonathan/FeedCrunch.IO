51e9677a50319987db078fea1408ffb7b4fac398
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2016-08-16 23:01:44 +0200

    Reset and Clear functions in Post creation and update forms fixed.

--- feedcrunch_rssadmin/static/js/ajax_form.js
+++ feedcrunch_rssadmin/static/js/ajax_form.js
@@ -45,6 +45,7 @@ $(function() {
   $("#clear").click( function(){
 	$("#title").val("");
 	$("#link").val("");
+	$(".tm-input").tagsManager('empty');
 	$("#activated_radio").bootstrapSwitch('state', true);
 	$("#twitter_radio").bootstrapSwitch('state', false);
 	$("#auto_format").bootstrapSwitch('state', false);
@@ -55,6 +56,8 @@ $(function() {
   $("#reset").click( function(){
 	$("#title").val($("#title").data("init"));
 	$("#link").val($("#link").data("init"));
+	$(".tm-input").tagsManager('empty');
+	$(".tm-input").tagsManager('pushTag',$("#tagInputField").data("init"));
 	$("#activated_radio").bootstrapSwitch('state', $("#activated_radio").data("init"));
 	$("#twitter_radio").bootstrapSwitch('state', false);
 	$("#auto_format").bootstrapSwitch('state', false);
