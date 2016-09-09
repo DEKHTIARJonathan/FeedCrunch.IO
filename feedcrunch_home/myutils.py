bb42836c11147134428b09df3031fa3753d9bf4d
DEKHTIAR Jonathan <contact@jonathandekhtiar.eu>
2016-09-09 12:30:08 +0200

    Template Footer Updated with Post_Count display instead of User_Count

--- feedcrunch_home/myutils.py
+++ feedcrunch_home/myutils.py
@@ -3,10 +3,12 @@
 
 from __future__ import unicode_literals
 from django.shortcuts import render
-from feedcrunch.models import FeedUser
+from feedcrunch.models import FeedUser, Post, Option
 
 def myrender(request, template, dictionary):
 	dictionary.update({'template_name': template})
 	dictionary.update({'user_count': FeedUser.objects.count()})
+	dictionary.update({'post_count': Post.objects.count()})
+	dictionary.update({'display_user_count': Option.objects.get(parameter="display_user_count").get_bool_value()})
 
 	return render(request, template, dictionary)
