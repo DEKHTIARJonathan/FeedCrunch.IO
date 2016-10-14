567c832348f7070f5cdbadd2c8fcf081fac35693
DEKHTIAR Jonathan <contact@jonathandekhtiar.eu>
2016-10-14 18:09:14 +0200

    wsgi fixed

--- application/wsgi.py
+++ application/wsgi.py
@@ -11,9 +11,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+
+import dotenv
+
+def load_env():
+    "Get the path to the .env file and load it."
+    project_dir = os.path.dirname(os.path.dirname(__file__))
+    dotenv.read_dotenv(os.path.join(project_dir, '.env'))
+
+load_env()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "application.settings")
 
