a51c73bc86c04d337d41ae8c7d47b9bf614c29d1
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2016-10-15 17:28:56 +0200

    Error Page Fixed

--- feedcrunch_home/views.py
+++ feedcrunch_home/views.py
@@ -82,17 +82,3 @@ def signUPView(request):
 		else:
 			country_list = Country.objects.all().order_by('name')
 			return render(request, 'signup.html', {'countries': country_list})
-
-#@login_required(login_url='/login/')
-def test(request):
-	return HttpResponse("Welcome.")
-
-def handler404(request):
-	response = render_to_response('404.html')
-	response.status_code = 404
-	return response
-
-def handler500(request):
-	response = render_to_response('500.html')
-	response.status_code = 500
-	return response
