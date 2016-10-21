8d3943c5f93a221c72bb5939ff2ce77deeeda21b
DEKHTIAR Jonathan <contact@jonathandekhtiar.eu>
2016-10-21 10:53:20 +0200

    Stop Scripts made cold start robust

--- codedeploy/Scripts/stop_server.sh
+++ codedeploy/Scripts/stop_server.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-screen -X -S django_app quit
-kill -9 `cat /home/ec2-user/feedcrunch/run/gunicorn.pid`
+ScreenName="django_app"
+if screen -list | grep -q $ScreenName; then
+    screen -X -S $ScreenName quit
+fi
+
+FILE="/home/ec2-user/feedcrunch/run/gunicorn.pid"
+if [ -f $FILE ]; then
+   echo "File '$FILE' Exists"
+   kill -9 `cat $FILE`
+fi
