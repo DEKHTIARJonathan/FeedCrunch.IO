30e2acb219ef53c3265c9fcbaa0ae2ea1d7183f4
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2016-11-10 02:10:10 +0100

    Error fix in Test

--- test_1.py
+++ test_1.py
@@ -2,5 +2,6 @@
 from datetime import timedelta
 from django.utils import timezone
 from django_q.tasks import schedule
+from django_q.models import Schedule
 
 schedule('feedcrunch.tasks.check_rss_subscribtion', username='dataradar', schedule_type=Schedule.ONCE, next_run=timezone.now() + timedelta(minutes=1))
