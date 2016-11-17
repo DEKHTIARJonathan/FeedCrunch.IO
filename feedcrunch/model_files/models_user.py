f8f849e2dec43ff656b9547e2791cd9781247538
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2016-11-17 02:54:21 +0100

    Error correct with import

--- feedcrunch/model_files/models_user.py
+++ feedcrunch/model_files/models_user.py
@@ -25,8 +25,6 @@ from twitter.tw_funcs import *
 
 from twython import Twython
 
-from feedcrunch.tasks import send_welcome_email
-
 from validators import ASCIIUsernameValidator, UnicodeUsernameValidator
 
 def generateDummyDesc():
@@ -141,6 +139,7 @@ class FeedUserManager(BaseUserManager):
 					user.save(using=self._db)
 
 					#schedule('feedcrunch.tasks.send_welcome_email', user_name=user.username, schedule_type=Schedule.ONCE, next_run=timezone.now() + datetime.timedelta(minutes=1))
+					from feedcrunch.tasks import send_welcome_email
 					send_welcome_email(user.username)
 
 					return user
