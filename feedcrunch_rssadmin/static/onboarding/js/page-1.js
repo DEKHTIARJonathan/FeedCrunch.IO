c1ecb31efbc30e7d9beb112e1ef8276caee640f2
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2016-11-22 02:09:26 +0100

    button continue fixed

--- feedcrunch_rssadmin/static/onboarding/js/page-1.js
+++ feedcrunch_rssadmin/static/onboarding/js/page-1.js
@@ -3,8 +3,17 @@ $( document ).ready(function() {
 
 	$('.interest-checkbox').on('change', function(evt) {
 		var limit = 3;
-		if($('.interest-checkbox:checked').length > limit) {
-			 this.checked = false;
+		var amount_checked = $('.interest-checkbox:checked').length;
+		if(amount_checked > limit) {
+			this.checked = false;
+		}
+		else if (amount_checked == limit) {
+			$(".continue").prop("disabled", false);
+			$(".continue").html("Continue &rarr;");
+		}
+		else{
+			$(".continue").prop("disabled", true);
+			$(".continue").html("Select 3 to Continue");
 		}
 	});
 
