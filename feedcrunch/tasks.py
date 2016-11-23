27486f3c80c86f9fe972081b2299080277f04008
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2016-11-23 12:42:41 +0100

    Tasks modified and fixed

--- feedcrunch/tasks.py
+++ feedcrunch/tasks.py
@@ -46,24 +46,21 @@ def welcome_mail(username):
   # this schedule will erase itself after having run
 
 def check_rss_feed(rss_id):
-	feed = RSSFeed.objects.get(id=rss_id)
-	feed.refresh_feed()
+	RSSFeed.objects.get(id=rss_id).refresh_feed()
 
-def check_user_rss_subscribtions(username):
-	usr = FeedUser.objects.get(username=username)
-
-	for feed in usr.rel_feeds.filter(active=True):
+def refresh_user_rss_subscribtions(username):
+	for feed in RSSFeed.objects.filter(rel_sub_feed_assoc__user=username):
 		schedule('feedcrunch.tasks.check_rss_feed', rss_id=feed.id, schedule_type=Schedule.ONCE, next_run=timezone.now() + timedelta(minutes=1))
 
-def check_allUsers_rss_subscribtions():
-	for user in FeedUser.objects.all():
-		schedule('feedcrunch.tasks.check_user_rss_subscribtions', username=user.username, schedule_type=Schedule.ONCE, next_run=timezone.now() + timedelta(minutes=1))
+def rss_all_rss_feeds():
+	for feed in RSSFeeds.objects.all():
+		schedule('feedcrunch.tasks.check_rss_feed', rss_id=feed.id, schedule_type=Schedule.ONCE, next_run=timezone.now() + timedelta(minutes=1))
 
 def launch_recurrent_rss_job():
 	execution_time = timezone.now()
 	execution_time = execution_time.replace(hour=3, minute=0, second=0, microsecond=0) + timedelta(days=1)
 
-	schedule('feedcrunch.tasks.check_allUsers_rss_subscribtions', schedule_type=Schedule.HOURLY, next_run=execution_time)
+	schedule('feedcrunch.tasks.rss_all_rss_feeds', schedule_type=Schedule.HOURLY, next_run=execution_time)
 
 def send_mass_welcome_email():
 	for user in FeedUser.objects.all():
