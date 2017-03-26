03ccc597ed2052a86dc56b31b3ff2f3c4cb979b4
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2017-03-26 15:27:35 +0200

    Update Working Bluemix

--- application/settings.py
+++ application/settings.py
@@ -107,7 +107,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
     'rest_framework',
     'encrypted_model_fields',
     'storages',
