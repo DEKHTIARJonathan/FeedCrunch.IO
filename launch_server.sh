4507e4773486cf1c12e905d49399709eea7da690
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2017-03-29 21:33:25 +0200

    Travis Script Updated

--- launch_server.sh
+++ launch_server.sh
@@ -11,7 +11,7 @@ python manage.py migrate
 python manage.py createcachetable
 #echo "from django.contrib.auth.models import User; User.objects.create_superuser('${USER}', '${MAIL}', '${PASS}')" | python manage.py shell
 
-echo [$0] Starting Django Server...
+echo [$0] Starting Django Server... 
 #python manage.py runserver 0.0.0.0:$SERVER_PORT --noreload
 
 NAME="FeedCrunch_Server"                                # Name of the application
