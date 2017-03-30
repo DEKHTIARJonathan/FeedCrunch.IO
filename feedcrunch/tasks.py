7ca281192ba84a3ae6d4fee3eb62b20f3b9cfbdb
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2017-03-30 09:50:55 +0200

    Fix with recurring tasks updating subscribers

--- feedcrunch/tasks.py
+++ feedcrunch/tasks.py
@@ -80,4 +80,4 @@ def launch_recurrent_rss_job():
     execution_time = execution_time.replace(hour=0, minute=0, second=0, microsecond=0) + timedelta(days=1)
 
     schedule('feedcrunch.tasks.refresh_all_rss_feeds', schedule_type=Schedule.HOURLY, next_run=execution_time)
-    schedule('feedcrunch.tasks.refresh_all_rss_subscribers', schedule_type=Schedule.DAILY, next_run=execution_time)
+    schedule('feedcrunch.tasks.refresh_all_rss_subscribers_count', schedule_type=Schedule.DAILY, next_run=execution_time)
