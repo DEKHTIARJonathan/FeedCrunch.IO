154847c3dc9a99e515b08f385db654c08ba16b59
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2017-04-10 00:39:28 +0200

    Exception Management Added to tasks.record_user_subscribers_stats

--- feedcrunch/task_files/task_record_rss_subscribers.py
+++ feedcrunch/task_files/task_record_rss_subscribers.py
@@ -35,8 +35,9 @@ def record_user_subscribers_stats(username=None):
 
             #count = usr.rel_rss_subscribers.filter(date__range=(last_lookup_day, today)).values("ipaddress").annotate(n=models.Count("pk")).count()__gte
             count = usr.rel_rss_subscribers.filter(date__gte=last_lookup_day).values("ipaddress").annotate(n=models.Count("pk")).count()
-
             RSSSubsStat.objects.create(user=username, count=count)
+        else:
+            raise Exception("Error: tasks.record_user_subscribers_stats - Already exist with datetime: " + str(timezone.now()) + " & date : " + str(timezone.now().date()))
 
     else:
         raise Exception("Error: tasks.record_user_subscribers_stats - username have not been provided.")
