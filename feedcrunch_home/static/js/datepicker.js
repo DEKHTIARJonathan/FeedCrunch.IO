ede5831ddd69fbbe12d5ac6e9c69f671c00c8dd7
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2017-09-01 12:20:54 +0200

    Signup Form Fixed

--- feedcrunch_home/static/js/datepicker.js
+++
@@ -1,9 +0,0 @@
-$('#sandbox-container .input-group.date').datepicker({
-	format: "dd/mm/yyyy",
-	startView: 3,
-	maxViewMode: 3,
-	todayBtn: true,
-	autoclose: true,
-	clearBtn: true,
-	defaultViewDate: { year: 1940, month: 04, day: 25 }
-});
