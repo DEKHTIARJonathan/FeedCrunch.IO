aed7fde78f84ea1d440bcc01df225fb8d750162c
DEKHTIARJonathan <contact@jonathandekhtiar.eu>
2018-04-02 11:59:22 +0200

    Log Files added to Prod Orchester. Commit to be reverted

--- scripts/bluemix/launch_orchester.sh
+++ scripts/bluemix/launch_orchester.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-celery beat -A application --loglevel=info --detach 
-celery events -A application --loglevel=info --camera=django_celery_monitor.camera.Camera --frequency=2.0 --detach
-celery worker -A application -l info --events
+celery beat -A application --loglevel=debug --detach --logfile=celerybeat.log
+celery events -A application --loglevel=debug --camera=django_celery_monitor.camera.Camera --frequency=2.0 --detach --logfile=celeryevents.log
+celery worker -A application -l debug --events --logfile=celeryworker.log
